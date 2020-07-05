c184ee1cc18287ff5ee54d91a71d647fb3875f27
natashagp <natasha.paulon@gmail.com>
2020-07-05 18:13:05 -0300

    app chat com flutter

---
+++ android/app/src/main/kotlin/com/natashapaulon/chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.natashapaulon.chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat/chat_screen.dart';
+import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        iconTheme: IconThemeData(color: Colors.blue),
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
